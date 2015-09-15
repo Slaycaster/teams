bcbf246dda88d5b99f115de12da9b6a3b05c919e
Jose Antonio Alvarez <jose.alvarez5522@gmail.com>
2015-09-15 22:42:18 +0800

    offline leave deduct
    sa transaction naka link para mapuntahan

--- app/views/create_requests/create.blade.php
+++ app/views/create_requests/create.blade.php
@@ -38,6 +38,7 @@
             <div class="label_white">{{ Form::label('end_date', 'End_Date:') }}</div>
             {{ Form::text('end_date', Input::get('end_date'), array('autocomplete' => 'off', 'size' => '35','id' => 'calendarb','placeholder' => 'yyyy-mm-dd')) }}<br>
             </div>
+            
          <div class = "col-md-3">
             <br><br>
             <div class="label_white">{{ Form::label('message', 'Message:') }}</div>
