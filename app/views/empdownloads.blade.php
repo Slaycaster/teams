a5bc7ffc82c0fe481bb2aefd35ebf3a9d6398274
Jose Antonio Alvarez <jose.alvarez5522@gmail.com>
2015-09-17 01:29:07 +0800

    layout
    this

--- app/views/empdownloads.blade.php
+++ app/views/empdownloads.blade.php
@@ -28,4 +28,20 @@
       @endforeach
       </div>
 
+<script type="text/javascript">
+    $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+    
+</script>
 @stop
